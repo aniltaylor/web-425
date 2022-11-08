2f14eee2f069c30db89f160f7946ace6546e4710
aniltaylor <aniltaylor@my365.bellevue.edu>
2022-11-08 17:41:42 -0600

    initial commit

---
+++ week-2/composer-app/src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+/*
+ Title: about.component.ts
+ Author: Professor Krasso
+ Modified By: Anita Taylor
+ Date: 10/26/2022
+ Description:  About component ( About view)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
