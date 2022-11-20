df7901fb4de946f3e806dbb699116ddee3b9bd42
aniltaylor <aniltaylor@my365.bellevue.edu>
2022-11-20 16:02:37 -0600

    hello

--- week-2/composer-app/src/app/app.component.ts
+++ week-2/composer-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  assignment = 'Assignment 2.4 - Routing in Action';
+  assignment = 'Composer App';
 }
