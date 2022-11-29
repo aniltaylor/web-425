e3b8c5472f65b0ae0aa1b562b952183036b842db
aniltaylor <aniltaylor@my365.bellevue.edu>
2022-11-28 23:08:15 -0600

    initial commit
    initial commit

--- week-4/di-composer-app/src/app/composer.service.ts
+++ week-4/di-composer-app/src/app/composer.service.ts
@@ -13,21 +13,21 @@ export class ComposerService {
 
 
 this.composers = [
-      {
-         composerId:100, fullName:"Tian Dun", genre:"Contemporary Classical"
-      },
-      {
-         composerId:101, fullName:"Johann Sebastian Bach", genre:"Classical"
-      },
-      {
-         composerId:102, fullName:"Antonio Vivaldi", genre:"Classical"
-      },
-      {
-         composerId:103, fullName:"Joseph Haydn", genre:"Classical"
-      },
-      {
-         composerId:104, fullName:"Wolfgang Amadeus Mozart", genre:"Classical"
-       }
+  {
+    composerId:100, fullName:"Fernando Sor", genre:"Contemporary Classical"
+ },
+ {
+    composerId:101, fullName:"Johann Sebastian Bach", genre:"Classical"
+ },
+ {
+    composerId:102, fullName:"Frederic Chopin", genre:"Classical"
+ },
+ {
+    composerId:103, fullName:"Joseph Haydn", genre:"Classical"
+ },
+ {
+    composerId:104, fullName:"Wolfgang Amadeus Mozart", genre:"Classical"
+  }
     ]
 
   }
