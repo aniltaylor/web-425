37be084079382027bd22758fba7c980051315cd3
aniltaylor <aniltaylor@my365.bellevue.edu>
2022-12-05 17:42:00 -0600

    initial comit

--- week-5/in-n-out-booksp1/src/app/app.component.spec.ts
+++ week-5/in-n-out-booksp1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'composer-app'`, () => {
+  it(`should have as title 'in-n-out-booksp1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('composer-app');
+    expect(app.title).toEqual('in-n-out-booksp1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('composer-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('in-n-out-booksp1 app is running!');
   });
 });
